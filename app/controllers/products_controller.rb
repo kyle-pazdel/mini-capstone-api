30c3cb66861da3fc100d534a5f7e093b46f5128d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-08-31 10:17:09 -0700

    Adds path to display available products to client in json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  def available_method
+    products = Product.all
+    available_products = Array.new
+    products.each do |product|
+      if product.availability == true
+        available_products << product
+      end
+    end
+    render json: available_products.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/available_products", controller: "products", action: "available_method"
+  get "/available_products.json", controller: "products", action: "available_method"
 end

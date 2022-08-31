4ffe9b993ac616ed0db46cc5dea6c00bfc2b9054
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-08-31 09:18:48 -0700

    Adds Product model with attributes to rails app

---
+++ db/migrate/20220831161806_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :description
+      t.boolean :availability
+
+      t.timestamps
+    end
+  end
+end

ae00a3092c2332e99b2a75e8f4acb2bb87f4e7a4
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-08-31 10:22:07 -0700

    Updates seeds to reflect current db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,25 +9,25 @@
 require "ffaker"
 
 Product.create(
-  name: FFaker::Product.product_name,
+  name: "Tag Filter",
   price: 50,
-  image_url: "TBD",
-  description: "TBD",
+  image_url: "https://ppolyzos.com/wp-content/uploads/2020/03/chrome-dev-tools-filters-include-exclude-1024x600.jpg",
+  description: "The Product Filter – Tags widget that is part of the WooCommerce Product Search extension provides a live product tag filter for your shop. While your customers choose the desired tag, it finds matching products and shows them on the shop page.",
   availability: true,
 )
 
 Product.create(
-  name: FFaker::Product.product_name,
+  name: "Air Output Bridge",
   price: 50,
-  image_url: "TBD",
-  description: "TBD",
+  image_url: "https://www.researchgate.net/profile/Rossano-Lang/publication/232087962/figure/fig2/AS:601719947923461@1520472623318/a-Overview-image-of-the-air-bridge-PC-fully-fabricated-with-FIB-b-central-region-of_Q320.jpg",
+  description: "Ideal for small and medium-sized networks, this Air Output Bridge brings a full slate of Cisco high-performance functionality to the enterprise environment.",
   availability: true,
 )
 Product.create(
-  name: FFaker::Product.product_name,
+  name: "Performance Case",
   price: 50,
-  image_url: "TBD",
-  description: "TBD",
+  image_url: "https://m.media-amazon.com/images/I/91zUa+-t1RL._AC_SS450_.jpg",
+  description: "Studio, cafe, stage, floor, coffee table—the 7U Performance case doesn't care where you play with it. Two 3U rows house and power all the modules you love (or one day hope to love), while the additional 1U row accommodates an extensive selection of specialized utility modules.",
   availability: true,
 )
 Product.create(
